d04e720403d96b54c7408f4c66775024d6f9e918
Tim Hawkins <56164332+illmade-knight@users.noreply.github.com>
2025-09-06 12:29:52 +0100

    aes support

--- pkg/transport/envelope.go
+++ pkg/transport/envelope.go
@@ -1,10 +1,15 @@
 package transport
 
-// SecureEnvelope is the data structure passed over the network.
-// It is understood by both the client and the routing-service.
 type SecureEnvelope struct {
-	SenderID         string `json:"sender_id"`
-	RecipientID      string `json:"recipient_id"`
-	EncryptedPayload []byte `json:"encrypted_payload"`
-	Signature        []byte `json:"signature"`
+	SenderID    string `json:"sender_id"`
+	RecipientID string `json:"recipient_id"`
+
+	// The AES-encrypted SharedPayload.
+	EncryptedData []byte `json:"encrypted_data"`
+
+	// The RSA-encrypted AES symmetric key.
+	EncryptedSymmetricKey []byte `json:"encrypted_symmetric_key"`
+
+	// The signature of the EncryptedData.
+	Signature []byte `json:"signature"`
 }
