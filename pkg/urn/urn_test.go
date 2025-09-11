f6a1e144ac1fd39ab50609fc873e5577453bb328
Tim Hawkins <56164332+illmade-knight@users.noreply.github.com>
2025-09-11 15:14:32 +0100

    fix test

--- pkg/urn/urn_test.go
+++ pkg/urn/urn_test.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/illmade-knight/routing-service/pkg/urn"
+	"github.com/illmade-knight/go-secure-messaging/pkg/urn"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
